85da52052a86ac56f7d4dfb6563794db8b925910
Pratik Nandan <pratiknandan007@gmail.com>
2023-12-26 21:18:15 +0530

    almost done

--- TaskHarbor/src/App.jsx
+++ TaskHarbor/src/App.jsx
@@ -10,36 +10,22 @@ function App() {
   const [isPending, setIsPending] = useState(true);
   const [isAddTask, setIsAddTask] = useState(false);
   const [screen, setScreen] = useState([false, false, false]);
-  const [description, setDescription] = useState(null);
+  const [description, setDescription] = useState(false);
+  const [sortBy, setSortBy] = useState(false);
 
   useEffect(() => {
     // Fetch data from JSON file (assuming it's in the public folder)
-   // handleResize();
+    // handleResize();
     fetch("/data.json")
       .then((response) => response.json())
       .then((data) => {
         setTasks(data);
       })
       .catch((error) => console.error("Error fetching data:", error));
-
-   
   }, []);
 
-  // const handleResize = () => {
-  //   if (window.innerWidth <= 390) {
-  //     setScreen(()=>[true, false, false]);
-  //   } else if (window.innerWidth <= 744) {
-  //     setScreen(()=>[false, true, false]);
-  //   } else {
-  //     setScreen(()=>[false, false, true]);
-  //   }
-  // };
-  // useEffect(() => {
-  //   window.addEventListener("resize", handleResize)
-  // })
-  
-
   const sortByCreateDate = () => {
+    setSortBy(false);
     const sortedTasks = [...tasks].sort((a, b) => {
       return new Date(a.createDate) - new Date(b.createDate);
     });
@@ -47,6 +33,7 @@ function App() {
   };
 
   const sortByDueDate = () => {
+    setSortBy(true);
     const sortedTasks = [...tasks].sort((a, b) => {
       return new Date(a.dueDate) - new Date(b.dueDate);
     });
@@ -65,123 +52,152 @@ function App() {
     const updatedTasks = tasks.filter((task) => task.createDate !== createDate);
     setTasks(updatedTasks);
   };
-  
- 
+
   return (
-    <div className="bg-white flex max-w-[480px] w-full items-stretch mx-auto pt-10">
+    <React.Fragment>
       {isAddTask && (
         <Model closeDialog={() => setIsAddTask(false)}>
-          <TaskForm />
+          <TaskForm closeDialog={() => setIsAddTask(false)} />
         </Model>
       )}
-    <div>
-      <header className="text-violet-800 text-4xl font-black whitespace-nowrap">
-        TO-DO LIST
-      </header>
+      <div
+        className={
+          `flex flex-col justify-center m-auto w-full sm:w-[90%] ` +
+          `${description ? "" : "sm:w-[60%]"}`
+        }
+      >
+        <header className="text-violet-800 text-4xl font-black whitespace-nowrap flex justify-center lg:justify-start ">
+          TO-DO LIST
+        </header>
 
-      <div className="self-center flex w-[487px] flex-col max-w-full items-stretch justify-between gap-5 mt-5 max-md:flex-wrap max-md:mt-10">
-        <div
-          className="flex justify-between gap-0 text-lg font-semibold 
-          whitespace-nowrap  items-stretch px-5 py-2
-         rounded-2xl border-indigo-800"
-        >
-          <div
-            className="text-violet-800 text-lg font-semibold whitespace-nowrap bg-indigo-100 justify-center items-stretch px-5 py-4 rounded-2xl"
-            aria-label="Sort by"
-          >
-            sort by
-          </div>
-
-          <div
-            className="text-black text-xl font-semibold self-center my-auto"
-            aria-label="Due date"
-            onClick={() => sortByCreateDate}
-          >
-            Date Created
-          </div>
-
-          <div
-            className="text-black text-xl font-semibold self-center my-auto"
-            onClick={() => sortByDueDate}
-            aria-label="Due date"
-          >
-            Due date
-          </div>
-        </div>
-
-        <div
-          onClick={() => setIsAddTask(true)}
-          className=" text-white  text-lg font-semibold whitespace-nowrap bg-violet-800 self-stretch justify-center items-center mt-4 px-16 py-2 rounded-2xl"
-        >
-          Create new task
-        </div>
-      </div>
-
-      <div className="flex w-full flex-col items-stretch mt-4 pt-6 color-#E5E4FF">
-        <div className="flex w-full flex-col pl-3 p-2 bg-indigo-100">
-          <div className="flex space-x-1">
-            <h2 className="text-black text-3xl font-bold self-stretch">
-              Pending Tasks
-            </h2>
+        <div className=" flex  flex-col mx-auto justify-between gap-5 mt-5 w-[70%]  md:flex-row md:justify-end md:w-full">
+          <div className="flex text-lg font-semibold rounded-2xl w-auto border-2 border-indigo-700 justify-center max-w-[294] md:max-w-full">
             <div
-              onClick={() => setIsAddTask(true)}
-              className="text-white text-lg font-semibold whitespace-nowrap bg-violet-800 self-stretch justify-center items-center mt-4 px-16 py-2 rounded-2xl"
+              className="text-violet-800 text-lg font-semibold whitespace-nowrap bg-indigo-100 justify-center items-stretch px-5 py-2 rounded-tl-2xl rounded-bl-2xl hidden md:block "
+              aria-label="Sort by"
             >
-              Create new task
+              sort by
+            </div>
+
+            <div
+              className={`text-black text-xl text-center font-semibold m-0 flex-grow px-2 py-2 rounded-tl-2xl rounded-bl-2xl md:rounded-none ${
+                !sortBy ? "bg-slate-400" : ""
+              }`}
+              aria-label="Due date"
+              onClick={() => sortByCreateDate()}
+            >
+              Date Created
+            </div>
+
+            <div
+              className={`text-black text-xl font-semibold text-center m-0 px-2 py-2 rounded-tr-2xl rounded-br-2xl flex-grow ${
+                sortBy ? "bg-slate-400" : ""
+              }`}
+              onClick={() => sortByDueDate()}
+              aria-label="Due date"
+            >
+              Due date
             </div>
           </div>
 
-          {tasks.map(
-            (task) =>
-              ((isPending && !task.isCompleted) ||
-                (!isPending && task.isCompleted)) && (
-                <div
-                  className="mx-1 bg-white self-stretch flex w-full flex-col items-stretch mt-3 pt-4 pb-2.5 px-2.5 rounded-2xl"
-                  key={task.createDate}
-                onClick={()=>setDescription(task)}>
-                  <div className="text-black text-xl font-semibold whitespace-nowrap overflow-hidden line-clamp-3">
-                    {task.title}
-                  </div>
-                  <div
-                    onClick={() => completeTask(task.createDate)}
-                    className="flex justify-between gap-5 mt-7 items-start"
-                  >
-                    {!task.isCompleted && (
-                      <button className="text-lime-700 text-center text-lg font-semibold whitespace-nowrap bg-lime-50 grow items-stretch pt-3.5 pb-1.5 px-9 rounded-md">
-                        Complete
-                      </button>
-                    )}
-                    <button
-                      onClick={deleteTask}
-                      className="text-red-800 text-center text-lg font-semibold whitespace-nowrap bg-rose-200 grow justify-center items-stretch px-12 py-3 rounded-md"
-                    >
-                      Delete
-                    </button>
-                  </div>
-                </div>
-              )
-          )}
+          <div
+            onClick={() => setIsAddTask(true)}
+            className="text-white text-lg font-semibold whitespace-nowrap bg-violet-800 self-stretch justify-center items-center  text-center py-2 rounded-2xl md:hidden lg:block md:px-12"
+          >
+            Create new task
+          </div>
         </div>
 
-        <footer className="bg-violet-50 bg-opacity-90 flex items-stretch justify-between gap-5 pr-12 rounded-none border-t-black border-t border-solid">
-          <div
-            onClick={() => setIsPending(true)}
-            className="text-black text-center text-xl font-bold whitespace-nowrap bg-slate-400 grow justify-center items-stretch px-14 py-5"
-          >
-            Pending
+
+     
+        <div className="flex w-full  mt-4 pt-6 color-#E5E4FF h-screen">
+          <div className={`flex w-full flex-col items-stretch  color-#E5E4FF `}>
+            <div
+              className={`flex w-full flex-col bg-indigo-100 justify-center p-3 rounded-tl-2xl rounded-tr-2xl ${
+                !description ? "" : "border-r-2 border-black rounded-tr-8xl"
+              }`}
+            >
+              <div className="flex items-center  justify-between m-2">
+                <h2 className="text-black text-3xl font-bold self-stretch items-center">
+                  Pending Tasks
+                </h2>
+
+                <div
+                  onClick={() => setIsAddTask(true)}
+                  className="text-white text-lg font-semibold whitespace-nowrap bg-violet-800 self-stretch justify-center items-center  px-16 py-2 rounded-2xl hidden md:block lg:hidden"
+                >
+                  Create new task
+                </div>
+              </div>
+
+              <div className="overflow-scroll ">
+                {tasks.map(
+                  (task) =>
+                    ((isPending && !task.isCompleted) ||
+                      (!isPending && task.isCompleted)) && (
+                      <div
+                        className="bg-white self-stretch flex w-full flex-col items-stretch mt-3 py-5 px-2.5 rounded-2xl md:flex-row lg:flex-row md:justify-between"
+                        key={task.createDate}
+                        onClick={() => setDescription(task)}
+                      >
+                        <div className="text-black text-xl font-semibold whitespace-nowrap overflow-hidden ">
+                          {task.title}
+                        </div>
+                        <div className="flex justify-between gap-5  items-center mt-4 md:mt-0 md:flex-col lg:flex-row">
+                          {!task.isCompleted && (
+                            <button
+                              onClick={() => completeTask(task.createDate)}
+                              className="text-lime-700 text-center text-lg font-semibold whitespace-nowrap bg-lime-50 grow items-stretch px-8 py-2 rounded-md"
+                            >
+                              Complete
+                            </button>
+                          )}
+                          <button
+                            onClick={deleteTask}
+                            className="text-red-800 text-center text-lg font-semibold whitespace-nowrap bg-rose-200 grow justify-center items-stretch px-11 py-2 rounded-md"
+                          >
+                            Delete
+                          </button>
+                        </div>
+                      </div>
+                    )
+                )}
+              </div>
+            </div>
+
+            <footer className="bg-violet-50 bg-opacity-90 flex items-stretch justify-between gap-5 pr-12 rounded-none border-t-black border-t border-solid">
+              <div
+                onClick={() => setIsPending(true)}
+                className={`text-black text-center text-xl font-bold whitespace-nowrap grow justify-center items-stretch px-14 py-5 ${
+                  isPending ? "bg-slate-400" : ""
+                }`}
+              >
+                Pending
+              </div>
+
+              <div
+                onClick={() => setIsPending(false)}
+                className={`text-black text-center text-xl font-bold whitespace-nowrap grow justify-center items-stretch px-14 py-5 ${
+                  !isPending ? "bg-slate-400" : ""
+                }`}
+              >
+                Completed
+              </div>
+            </footer>
           </div>
-          <div
-            onClick={() => setIsPending(false)}
-            className="text-black text-center text-xl font-semibold my-auto"
-          >
-            Completed
-          </div>
-        </footer>
+
+          {window.innerWidth>=769&&description && <Description data={description} border={true} />}
+        </div>
+
       </div>
-      </div>
-      {description&&window.innerWidth<744&&<Model closeDialog={() => setDescription(null)}><Description data={description} border={false}/></Model>}
-      {description&&window.innerWidth>=744&&<Description data={description} border={true}/>}
-    </div>
+     
+        {window.innerWidth<769&&description && (
+          <Model closeDialog={() => setDescription(null)}>
+            <Description data={description} border={false} closeDialog={() => setDescription(null)} />
+          </Model>
+        )}
+     
+    </React.Fragment>
   );
 }
 
--- TaskHarbor/src/components/TaskForm.jsx
+++ TaskHarbor/src/components/TaskForm.jsx
@@ -1,52 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 
-function TaskForm() {
+function TaskForm(props) {
+  // State variables for date and image
+  const [dueDate, setDueDate] = useState(Date("15/11/2023"));
+  const [image, setImage] = useState(null); // Change this to the actual type you'll use for image data
+
+  // Event handlers for updating state variables
+  const handleDueDateChange = (event) => {
+    setDueDate(event.target.value);
+  };
+
+  const handleImageChange = (event) => {
+    // Handle image upload and set the appropriate state
+    // For simplicity, assuming a file input for image
+    const selectedImage = event.target.files[0];
+    setImage(selectedImage);
+  };
+
   return (
-    <form className="bg-white flex max-w-[390px] flex-col items-stretch pl-7 pr-4 pt-9 pb-6 rounded-2xl">
-      <header className="text-violet-800 text-4xl font-semibold">New Task</header>
-      <div className="text-black text-xl font-light whitespace-nowrap bg-indigo-100 justify-center mt-10 pl-6 pr-16 py-6 rounded-2xl items-start">
-        <label htmlFor="title" className="sr-only">
-          Title
-        </label>
+    <form className="bg-white flex flex-col justify-between items-stretch p-3 rounded-2xl h-full">
+      <header className="text-violet-800 text-4xl font-semibold mb-6">
+        New Task
+      </header>
+
+      <input
+        className="text-black text-xl font-light whitespace-nowrap bg-indigo-100 mt-2 p-3 rounded-2xl"
+        type="text"
+        id="title"
+        placeholder="Title"
+        aria-label="Title"
+        aria-required="true"
+      />
+
+      <textarea
+        id="description"
+        className="text-black text-xl font-light whitespace-nowrap bg-indigo-100 mt-4 p-3 rounded-2xl"
+        placeholder="Description..."
+        aria-label="Description"
+        aria-required="true"
+        rows="4"
+      ></textarea>
+
+<div className="flex items-center justify-between mt-4 p-3 rounded-2xl bg-indigo-100">
+  {/* Your custom-styled "Change" button */}
+  <label htmlFor="dueDate" className="custom-date-input-button button bg-neutral-500 p-3 rounded-2xl cursor-pointer">
+    Change
+  </label>
+
+  {/* Your visually hidden date input */}
+  <input
+    className="text-black text-xl font-light whitespace-nowrap bg-indigo-100 p-3 rounded-2xl appearance-none border-none absolute top-0 left-0 opacity-0"
+    type="date"
+    id="dueDate"
+    placeholder={dueDate}
+    value={dueDate}
+    onChange={handleDueDateChange}
+  />
+</div>
+
+
+      <div className="flex items-end justify-between mt-4 rounded-2xl bg-indigo-100">
+        {/* Your file input */}
         <input
-          type="text"
-          id="title"
-          className="input"
-          placeholder="Title"
-          aria-label="Title"
-          aria-required="true"
+          className="text-black text-xl font-light whitespace-nowrap"
+          type="file"
+          id="image"
+          accept="image/*"
+          placeholder="No Image"
+          aria-label="Image"
+          onChange={handleImageChange}
+          style={{ display: "none" }} // hide the default file input
         />
-      </div>
-      <div className="text-black text-xl font-light whitespace-nowrap bg-indigo-100 aspect-square mt-2 pl-6 pr-16 pt-7 pb-64 rounded-2xl items-start">
-        <label htmlFor="description" className="sr-only">
-          Description
+
+        {/* Your custom-styled "Choose File" button */}
+        <label
+          htmlFor="image"
+          className="custom-file-input button bg-neutral-500 p-3 rounded-r-2xl ml-auto "
+        >
+          Choose File
         </label>
-        <textarea
-          id="description"
-          className="input"
-          placeholder="Description..."
-          aria-label="Description"
-          aria-required="true"
-          rows="4"
-        ></textarea>
       </div>
-      <div className="bg-indigo-100 flex items-center justify-between gap-3.5 mt-2 pl-6 rounded-2xl">
-        <div className="text-black text-xl font-light grow whitespace-nowrap my-auto">
-          Due on: 15-11-2023
-        </div>
-        <button className="button bg-neutral-500 pl-5 pr-8 py-6 rounded-none">
-          Change
+
+      <div className="flex items-center justify-between mt-4 flex-grow">
+        <button
+          className=" py-4 w-[48%] font-medium border border-transparent rounded-md hover:bg-violet-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-violet-500"
+          onClick={() => {
+            props.closeDialog();
+          }}
+        >
+          Cancel
         </button>
-      </div>
-      <div className="bg-indigo-100 flex items-center justify-between gap-5 mt-2 pl-7 rounded-2xl">
-        <div className="text-black text-xl font-light my-auto">No image...</div>
-        <button className="button bg-neutral-500 px-3 py-6 rounded-none">
-          Add image
-        </button>
-      </div>
-      <div className="flex items-center justify-between gap-5 mt-8 px-0.5">
-       
-        <button className="button bg-violet-800 pl-12 pr-10 py-6 rounded-2xl">
+        <button className="w-[48%] py-4 font-medium text-white bg-violet-800 border border-transparent rounded-md hover:bg-violet-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-violet-500">
           Save
         </button>
       </div>
